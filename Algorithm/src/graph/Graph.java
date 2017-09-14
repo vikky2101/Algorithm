a0e0579a697ab0c46f547126cc84fc01c8f2a5f5
Vikash Kumar Gautam <VikashKumar_Gautam@amat.com>
2017-09-15 01:06:03 +0530

    Algo2

--- Algorithm/src/graph/Graph.java
+++ Algorithm/src/graph/Graph.java
@@ -7,96 +7,166 @@ import java.util.Iterator;
 
 public class Graph {
 
-	private static final int V = 9;
-	private int numberOfVertices;
+	private int no_vertex;
 	private LinkedList<Integer> adjList[];
+	private int V = 5;
+	private int V2 = 9;
 
-	public Graph(int n) {
-		numberOfVertices = n;
-		adjList = new LinkedList[n];
-		for (int i = 0; i < n; i++) {
+	public Graph(int num) {
+		this.no_vertex = num;
+		adjList = new LinkedList[num];
+		for (int i = 0; i < num; i++) {
 			adjList[i] = new LinkedList<Integer>();
 		}
 	}
 
-	public void addEdge(int u, int v) {
-		adjList[u].add(v);
+	private void addEdge(int src, int des) {
+		adjList[src].add(des);
 	}
 
-	private void BFSUtil(int source, boolean[] arr, Queue<Integer> queue) {
-		arr[source] = true;
+	private void printBFS(int source) {
+		boolean[] visit = new boolean[no_vertex];
+		for (int i = 0; i < no_vertex; i++)
+			visit[i] = false;
+
+		Queue<Integer> queue = new LinkedList<>();
+
+		for (int i = 0; i < no_vertex; i++) {
+			if (!visit[i])
+				pritnBFSUtil(queue, visit, i);
+		}
+	}
+
+	private void pritnBFSUtil(Queue<Integer> queue, boolean[] visit, int source) {
+		visit[source] = true;
 		queue.add(source);
 		while (!queue.isEmpty()) {
 			int temp = queue.poll();
 			System.out.print(temp + " ");
-			Iterator<Integer> itr = adjList[temp].listIterator();
+			Iterator<Integer> itr = adjList[temp].iterator();
 			while (itr.hasNext()) {
 				int v = itr.next();
-				if (!arr[v]) {
-					arr[v] = true;
+				if (!visit[v]) {
+					visit[v] = true;
 					queue.add(v);
 				}
 			}
 		}
 	}
 
-	public void printBFS(int source) {
-		boolean arr[] = new boolean[numberOfVertices];
-		Queue<Integer> queue = new LinkedList<>();
-		for (int i = 0; i < numberOfVertices; i++) {
-			if (!arr[i])
-				BFSUtil(i, arr, queue);
+	private void printDFS(int source) {
+		boolean[] visit = new boolean[no_vertex];
+		for (int i = 0; i < no_vertex; i++)
+			visit[i] = false;
+
+		for (int i = 0; i < no_vertex; i++) {
+			if (!visit[i])
+				pritnDFSUtil(visit, i);
 		}
 	}
 
-	private void DFSUtil(int source, boolean[] arr) {
-		arr[source] = true;
+	private void pritnDFSUtil(boolean[] visit, int source) {
+		visit[source] = true;
 		System.out.print(source + " ");
-		Iterator<Integer> itr = adjList[source].listIterator();
+		Iterator<Integer> itr = adjList[source].iterator();
 		while (itr.hasNext()) {
 			int v = itr.next();
-			if (!arr[v]) {
-				DFSUtil(v, arr);
+			if (!visit[v]) {
+				pritnDFSUtil(visit, v);
 			}
 		}
 	}
 
-	public void printDFS() {
-		boolean arr[] = new boolean[numberOfVertices];
-		for (int i = 0; i < numberOfVertices; i++) {
-			if (!arr[i])
-				DFSUtil(i, arr);
+	private boolean isCycleDirected() {
+		boolean visit[] = new boolean[no_vertex];
+		boolean recStack[] = new boolean[no_vertex];
+		for (int i = 0; i < no_vertex; i++) {
+			if (isCycleDirectedUtil(visit, recStack, i))
+				return true;
 		}
+		return false;
 	}
 
-	public void tropoUtil(int source, boolean[] arr, Stack<Integer> stack) {
-		arr[source] = true;
-		Iterator<Integer> itr = adjList[source].listIterator();
+	private boolean isCycleDirectedUtil(boolean[] visit, boolean[] recStack, int source) {
+		if (visit[source] == false) {
+			visit[source] = true;
+			recStack[source] = true;
+			Iterator<Integer> itr = adjList[source].iterator();
+			while (itr.hasNext()) {
+				int v = itr.next();
+				if (!visit[v] && isCycleDirectedUtil(visit, recStack, v))
+					return true;
+				else if (recStack[v])
+					return true;
+			}
+		}
+		recStack[source] = false;
+		return false;
+	}
+
+	private boolean isCycleUnDirected() {
+		boolean visit[] = new boolean[no_vertex];
+		for (int i = 0; i < no_vertex; i++) {
+			if (isCycleUnDirectedUtil(visit, i, -1))
+				return true;
+		}
+		return false;
+	}
+
+	private boolean isCycleUnDirectedUtil(boolean[] visit, int source, int parent) {
+		if (visit[source] == false) {
+			visit[source] = true;
+			Iterator<Integer> itr = adjList[source].iterator();
+			while (itr.hasNext()) {
+				int v = itr.next();
+				if (!visit[v] && isCycleUnDirectedUtil(visit, v, v))
+					return true;
+				else if (v != parent)
+					return true;
+			}
+		}
+		return false;
+	}
+
+	private void troposort() {
+		Stack stack = new Stack<>();
+		boolean visit[] = new boolean[no_vertex];
+		for (int i = 0; i < no_vertex; i++) {
+			if (!visit[i])
+				troposortUtil(stack, visit, i);
+		}
+
+		while (!stack.isEmpty())
+			System.out.print(stack.pop() + " ");
+
+	}
+
+	private void troposortUtil(Stack stack, boolean visit[], int source) {
+		visit[source] = true;
+		Iterator<Integer> itr = adjList[source].iterator();
 		while (itr.hasNext()) {
 			int v = itr.next();
-			if (!arr[v]) {
-				tropoUtil(v, arr, stack);
-			}
+			if (!visit[v])
+				troposortUtil(stack, visit, v);
 		}
 		stack.push(source);
 	}
 
-	public void tropologicalSorting(int source) {
-		boolean arr[] = new boolean[numberOfVertices];
-		Stack<Integer> stack = new Stack<Integer>();
-		for (int i = 0; i < numberOfVertices; i++) {
-			if (!arr[i])
-				tropoUtil(i, arr, stack);
+	private boolean isGraphTree() {
+		boolean visit[] = new boolean[no_vertex];
+		if (isCycleUnDirectedUtil(visit, 0, -1))
+			return false;
+		for (int i = 0; i < no_vertex; i++) {
+			if (!visit[i])
+				return false;
 		}
-		while (!stack.isEmpty()) {
-			System.out.print(stack.pop() + " ");
-		}
+		return true;
 	}
 
-	private int minKey(int key[], boolean mst[]) {
+	private int minKey(int key[], boolean mst[], int no) {
 		int minIndex = -1;
 		int min = 9999;
-		for (int i = 0; i < V; i++) {
+		for (int i = 0; i < no; i++) {
 			if (mst[i] == false && key[i] < min) {
 				min = key[i];
 				minIndex = i;
@@ -105,66 +175,125 @@ public class Graph {
 		return minIndex;
 	}
 
-	public void primeMST(int graph[][]) {
+	private void primsMST(int graph[][]) {
+		boolean mstSet[] = new boolean[V];
+		int keySet[] = new int[V];
 		int parent[] = new int[V];
-		boolean mst[] = new boolean[V];
-		int key[] = new int[V];
-
 		for (int i = 0; i < V; i++) {
-			mst[i] = false;
+			mstSet[i] = false;
 			parent[i] = -1;
-			key[i] = 99999;
+			keySet[i] = Integer.MAX_VALUE;
 		}
-		key[0] = 0;
+		keySet[0] = 0;
 		parent[0] = -1;
 
 		for (int count = 0; count < V - 1; count++) {
-			int minKey = minKey(key, mst);
-			mst[minKey] = true;
-			for (int i = 0; i < V; i++) {
-				if (graph[minKey][i] != 0 && mst[i] == false && graph[minKey][i] < key[i]) {
-					key[i] = graph[minKey][i];
-					parent[i] = minKey;
+			int minKey = minKey(keySet, mstSet, V);
+			mstSet[minKey] = true;
+			for (int j = 0; j < V; j++) {
+				if (graph[minKey][j] != 0 && mstSet[j] == false && graph[minKey][j] < keySet[j]) {
+					keySet[j] = graph[minKey][j];
+					parent[j] = minKey;
 				}
 			}
 		}
-		// print MST
+
 		for (int i = 1; i < V; i++) {
-			System.out.println("Edge-" + i + "," + parent[i] + " Weight-" + graph[i][parent[i]]);
+			System.out.println("Edge-" + parent[i] + i + " Weight-" + graph[parent[i]][i]);
 		}
+
 	}
 
-	public void Dijkstra(int graph[][], int src) {
-		int parent[] = new int[V];
-		boolean mst[] = new boolean[V];
-		int key[] = new int[V];
-
-		for (int i = 0; i < V; i++) {
-			mst[i] = false;
+	private void dijkstra(int[][] graph, int source) {
+		boolean mstSet[] = new boolean[V2];
+		int keySet[] = new int[V2];
+		int parent[] = new int[V2];
+		for (int i = 0; i < V2; i++) {
+			mstSet[i] = false;
 			parent[i] = -1;
-			key[i] = 99999;
+			keySet[i] = Integer.MAX_VALUE;
 		}
+		keySet[source] = 0;
+		parent[source] = -1;
 
-		key[src] = 0;
-		parent[src] = -1;
-
-		for (int count = 0; count < V - 1; count++) {
-
-			int minKey = minKey(key, mst);
-			mst[minKey] = true;
-			for (int i = 0; i < V; i++) {
-				if (graph[minKey][i] != 0 && mst[i] == false && key[minKey] != 9999
-						&& key[minKey] + graph[minKey][i] < key[i]) {
-					key[i] = key[minKey] + graph[minKey][i];
-					parent[i] = minKey;
+		for (int count = 0; count < V2 - 1; count++) {
+			int minKey = minKey(keySet, mstSet, V2);
+			mstSet[minKey] = true;
+			for (int j = 0; j < V2; j++) {
+				if (graph[minKey][j] != 0 && mstSet[j] == false && keySet[minKey] != Integer.MAX_VALUE
+						&& keySet[minKey] + graph[minKey][j] < keySet[j]) {
+					keySet[j] = graph[minKey][j] + keySet[minKey];
+					parent[j] = minKey;
 				}
 			}
 		}
 
-		System.out.println("Distance of all Vertices from Src");
-		System.out.println("Vertex  - Distance from Src");
-		for (int i = 0; i < V; i++) {
-			System.out.println(i + " " + key[i]);
+		for (int i = 1; i < V2; i++) {
+			System.out.println("Key-" + i + " DIstance-" + keySet[i]);
 		}
+
 	}
+
+	public static void main(String args[]) {
+		// Graph start
+		Graph graph = new Graph(5);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 4);
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 0);
+		graph.addEdge(3, 3);
+		System.out.println("");
+		System.out.print("BFS - ");
+		graph.printBFS(2);
+		System.out.println("");
+		System.out.print("DFS - ");
+		graph.printDFS(0);
+		System.out.println("");
+		if (graph.isCycleDirected())
+			System.out.println("Cycle exists");
+		else
+			System.out.println("Cycle not exists");
+		if (graph.isCycleUnDirected())
+			System.out.println("Cycle exists");
+		else
+			System.out.println("Cycle not exists");
+		if (graph.isGraphTree())
+			System.out.println("Graph is tree");
+		else
+			System.out.println("Graph is not tree");
+
+		Graph g = new Graph(6);
+		g.addEdge(5, 2);
+		g.addEdge(5, 0);
+		g.addEdge(4, 0);
+		g.addEdge(4, 1);
+		g.addEdge(2, 3);
+		g.addEdge(3, 1);
+		System.out.print("TropLogical Sorting - ");
+		g.troposort();
+		System.out.println("");
+		System.out.println("Prims MST - ");
+
+		// print MST
+		int mstgraph[][] = new int[][] { { 0, 2, 0, 6, 0 }, { 2, 0, 3, 8, 5 }, { 0, 3, 0, 0, 7 }, { 6, 8, 0, 0, 9 },
+				{ 0, 5, 7, 9, 0 }, };
+
+		System.out.println("");
+		// Print the solution
+		graph.primsMST(mstgraph);
+		 int dgraph[][] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
+                 {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                 {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                 {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                 {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                 {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                 {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                 {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                 {0, 0, 2, 0, 0, 0, 6, 7, 0}};
+		System.out.println("");
+		System.out.println("Dijkstra Algo");
+		graph.dijkstra(dgraph, 0);
+
+	}
+
 }
--- Algorithm/src/main/Main.java
+++ Algorithm/src/main/Main.java
@@ -1,74 +1,20 @@
 package main;
-import graph.Graph;
+
 import math.LexicographicalRank;
 import math.Mathematical_Algo;
-import backtracking.KnightTourProblem;
-import backtracking.NQueenProblem;
-import backtracking.PrintAllPermutStrings;
-import dynamicprogramming.DP;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		// Graph start
-		Graph graph = new Graph(4);
-		graph.addEdge(0, 1);
-		graph.addEdge(0, 2);
-		graph.addEdge(1, 2);
-		graph.addEdge(2, 0);
-		graph.addEdge(2, 3);
-		graph.addEdge(3, 3);
-		System.out.print("DFS - ");
-		graph.printDFS();
-		System.out.println("");
-		System.out.print("BFS - ");
-		graph.printBFS(2);
-		Graph g = new Graph(6);
-	    g.addEdge(5, 2);
-	    g.addEdge(5, 0);
-	    g.addEdge(4, 0);
-	    g.addEdge(4, 1);
-	    g.addEdge(2, 3);
-	    g.addEdge(3, 1);
-		System.out.println("");
-		System.out.println("TropLogical Sorting - ");
-		g.tropologicalSorting(2);
-		System.out.println("");
-		System.out.println("Prims MST - ");
-		
-		
-		// print MST
-	     int mstgraph[][] = new int[][]{{0, 4, 0, 0, 0, 0, 0, 8, 0},
-             {4, 0, 8, 0, 0, 0, 0, 11, 0},
-             {0, 8, 0, 7, 0, 4, 0, 0, 2},
-             {0, 0, 7, 0, 9, 14, 0, 0, 0},
-             {0, 0, 0, 9, 0, 10, 0, 0, 0},
-             {0, 0, 4, 14, 10, 0, 2, 0, 0},
-             {0, 0, 0, 0, 0, 2, 0, 1, 6},
-             {8, 11, 0, 0, 0, 0, 1, 0, 7},
-             {0, 0, 2, 0, 0, 0, 6, 7, 0}
-            };
-	 
-            System.out.println("");
-	        // Print the solution
-	       graph.primeMST(mstgraph);
-	       System.out.println("");
-	       System.out.println("Dijkstra Algo");
-	      // Dijkstra Algo           
-	        graph.Dijkstra(mstgraph, 0);
-	        
-	        // Graph end
-	        
-	        
-	        //Maths Algo
-	        String number = "534976";
-	        int n = number.length();
-	        System.out.print(number+" ");
-	        Mathematical_Algo.nextGreaterNumberSameDigit(number.toCharArray(), n);
-	        
-	        //Lexigo rank
-	        LexicographicalRank lex = new LexicographicalRank();
-	        System.out.println("Lexical Rank " + lex.findRank("string"));    
+
+		// Maths Algo
+		String number = "534976";
+		int n = number.length();
+		System.out.print(number + " ");
+		Mathematical_Algo.nextGreaterNumberSameDigit(number.toCharArray(), n);
+
+		// Lexigo rank
+		LexicographicalRank lex = new LexicographicalRank();
+		System.out.println("Lexical Rank " + lex.findRank("string"));
 	}
 }
